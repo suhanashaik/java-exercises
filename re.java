e3bfd70355b2dfd9b76e8b171fce7f7b3ed74290
suhanashaik <shaiksuhana.sonu@gmail.com>
2016-10-06 20:04:57 +0530

    Add files via upload

---
+++ re.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Re
+{
+public static void main(String args[])
+{
+String[] array={"one","two","three"};
+ArrayList array=new ArrayList();
+array.clear();
+array.add(1,"0th element");
+array.add(2,"1st element");
+System.out.println(array);
+}
+}
